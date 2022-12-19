64bb4f0eef99c9e8ee88c8167fb7b4305ec9e6a6
szejnaArtur <50076342+szejnaArtur@users.noreply.github.com>
2022-12-19 20:45:37 +0100

    Menu added.

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,65 +1,60 @@
-import {useState, useEffect} from "react";
-
 import classes from './main-navigation.module.css';
+import Image from "next/image";
+import {useState} from "react";
 import Link from "next/link";
 
-const MainNavigation = () => {
-    const [nav, setNav] = useState(false);
-    const [color, setColor] = useState('transparent');
-    const [textColor, setTextColor] = useState('#fcfcfc');
+import {BiLogOut, BiMessageAltDetail, BiNotification, BiSupport, BiUser} from 'react-icons/bi';
+import {FiSettings} from 'react-icons/fi';
 
-    const handleNav = () => {
-        setNav(!nav);
+const MainNavigation = () => {
+
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(prevState => !prevState);
     }
 
-    useEffect(() => {
-        const changeColor = () => {
-
-            if (window.scrollY >= 90) {
-                setColor('#fcfcfc');
-                setTextColor('#2f2f2f');
-            } else {
-                setTextColor('#fcfcfc');
-                setColor('transparent');
-            }
-        }
-        window.addEventListener('scroll', changeColor);
-    }, []);
-
     return (
-        <div
-            style={
-                textColor === '#fcfcfc' ? {
-                    backgroundColor: `${color}`,
-                    boxShadow: 'none'
-                } : {
-                    backgroundColor: `${color}`,
-                    boxShadow: '0 0.3rem 1.9rem #000'
-                }
-            }
-            className={classes.container}
-        >
-            <div className={classes.content}>
-                <Link href='/'>
-                    <h1>Count Macro</h1>
-                </Link>
-                <ul className={classes.links} style={{color: `${textColor}`}}>
-                    <li className={classes.link}>
-                        <Link href='/' scroll={true}>Home</Link>
-                    </li>
-                    <li className={classes.link}>
-                        <Link href='/about' scroll={true}>About</Link>
-                    </li>
-                </ul>
-                <ul className={classes.btnContainer}>
-                    <li className={classes.btn}>
-                        <Link href='/login'>Login</Link>
-                    </li>
-                    <li className={classes.btn}>
-                        <Link href='/login'>Register</Link>
-                    </li>
-                </ul>
+        <div className={active ? classes.navigation : classes.active}>
+            <div className={classes.userBx}>
+                <div className={classes.imgBx}>
+                    <Image src='/images/user/male.png' alt='/' width={60} height={60} className={classes.img}/>
+                </div>
+                <p className={classes.username}>Username</p>
             </div>
+            <div className={classes.menuToggle} onClick={handleClick}></div>
+            <ul className={classes.menu}>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <BiUser className={classes.icon}/> My Profile
+                    </Link>
+                </li>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <BiMessageAltDetail className={classes.icon}/> Messages
+                    </Link>
+                </li>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <BiNotification className={classes.icon}/>Notification
+                    </Link>
+                </li>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <FiSettings className={classes.icon}/>Settings
+                    </Link>
+                </li>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <BiSupport className={classes.icon}/>Help & Support
+                    </Link>
+                </li>
+                <li>
+                    <Link href='#' className={classes.link}>
+                        <BiLogOut className={classes.icon}/>Logout
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
